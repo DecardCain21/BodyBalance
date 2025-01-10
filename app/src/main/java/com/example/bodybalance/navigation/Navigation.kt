fabb0de2520d495d0eb389d47f0ef99220d28d10
Nikita <trenin.0235@gmail.com>
2025-01-10 22:03:10 +0700

    test

---
+++ app/src/main/java/com/example/bodybalance/navigation/Navigation.kt
@@ -0,0 +1,11 @@
+package com.example.bodybalance.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun Navigation() {
+    val navController: NavHostController = rememberNavController()
+
+}
