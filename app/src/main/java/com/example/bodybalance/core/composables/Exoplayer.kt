f4a332fe3a0b79ff30c9c048bc2e04610644412a
Nikita <trenin.0235@gmail.com>
2025-02-10 21:02:56 +0700

    Fullscreen

--- app/src/main/java/com/example/bodybalance/core/composables/Exoplayer.kt
+++ app/src/main/java/com/example/bodybalance/core/composables/Exoplayer.kt
@@ -2,15 +2,17 @@ package com.example.bodybalance.core.composables
 
 import android.app.Activity
 import android.content.Context
+import android.content.ContextWrapper
+import android.content.pm.ActivityInfo
 import android.content.res.Configuration
 import androidx.annotation.OptIn
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableLongStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
@@ -36,21 +38,13 @@ fun ExoPlayer(
     modifier: Modifier = Modifier,
     url: String = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
 ) {
-
     val localContext = LocalContext.current
-
     val lifecycleOwner = androidx.lifecycle.compose.LocalLifecycleOwner.current
-
     var currentPosition by rememberSaveable { mutableLongStateOf(0L) }
-
     val exoPlayer = remember { createConfiguredExoPlayer(localContext, url, currentPosition) }
 
     val configuration = LocalConfiguration.current
-    val isLandscape by remember {
-        derivedStateOf {
-            configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
-        }
-    }
+    var isLandscape by remember { mutableStateOf(configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) }
     val activity = localContext as Activity
 
     DisposableEffect(lifecycleOwner) {
@@ -61,7 +55,6 @@ fun ExoPlayer(
                 }
 
                 Lifecycle.Event.ON_RESUME -> exoPlayer.playWhenReady = true
-
                 else -> Unit
             }
         }
@@ -73,7 +66,7 @@ fun ExoPlayer(
         }
     }
 
-    DisposableEffect(isLandscape) {
+    /*DisposableEffect(isLandscape) {
         val windowInsetsController =
             WindowCompat.getInsetsController(activity.window, activity.window.decorView)
 
@@ -90,28 +83,72 @@ fun ExoPlayer(
         onDispose {
             windowInsetsController.show(WindowInsetsCompat.Type.systemBars())
         }
-    }
+    }*/
 
     AndroidView(
-        modifier = modifier
+        modifier = Modifier
             .aspectRatio(16 / 9f),
         factory = { context ->
             PlayerView(context).apply {
+                setFullscreenButtonClickListener { isFullScreen ->
+                    with(context) {
+                        if (isFullScreen) {
+                            val windowInsetsController =
+                                WindowCompat.getInsetsController(activity.window, activity.window.decorView)
+                            windowInsetsController.systemBarsBehavior =
+                                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+                            setScreenOrientation(orientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
+                        } else {
+                            setScreenOrientation(orientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
+                        }
+                    }
+                }
                 resizeMode = RESIZE_MODE_ZOOM
                 player = exoPlayer
             }
         },
-        update = {
-            it.player = exoPlayer
-        },
+        update = { it.player = exoPlayer }
     )
+
 }
 
-/* Соотношения сторон */
-// 9:16 -  Полноэкранное вертикальное видео
-// 3:4 - Более компактное вертикальное видео
-// 1:1 - Квадратное видео
-// 16:9 -  Горизонтальное видео
+fun Context.findActivity(): Activity? = when (this) {
+    is Activity -> this
+    is ContextWrapper -> baseContext.findActivity()
+    else -> null
+}
+
+fun Context.setScreenOrientation(orientation: Int) {
+    val activity = this.findActivity() ?: return
+    activity.requestedOrientation = orientation
+    if (orientation == ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE) {
+        hideSystemUi()
+    } else {
+        showSystemUi()
+    }
+}
+
+fun Context.hideSystemUi() {
+    val activity = this.findActivity() ?: return
+    val window = activity.window ?: return
+    WindowCompat.setDecorFitsSystemWindows(window, false)
+    WindowInsetsControllerCompat(window, window.decorView).let { controller ->
+        controller.hide(WindowInsetsCompat.Type.systemBars())
+        controller.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+    }
+}
+
+fun Context.showSystemUi() {
+    val activity = this.findActivity() ?: return
+    val window = activity.window ?: return
+    WindowCompat.setDecorFitsSystemWindows(window, true)
+    WindowInsetsControllerCompat(
+        window,
+        window.decorView
+    ).show(WindowInsetsCompat.Type.systemBars())
+}
+
 private fun createConfiguredExoPlayer(
     context: Context,
     url: String,
