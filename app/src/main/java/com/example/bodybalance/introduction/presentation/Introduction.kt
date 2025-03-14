4616a309282bf1495ddf00ef78e88288d93123b3
Cain <decardcain21@gmail.com>
2025-03-14 16:23:49 +0300

    introduction ui minor changes

--- app/src/main/java/com/example/bodybalance/introduction/presentation/Introduction.kt
+++ app/src/main/java/com/example/bodybalance/introduction/presentation/Introduction.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -18,14 +19,21 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalInspectionMode
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.media3.common.Player
 import androidx.media3.common.util.UnstableApi
+import com.example.bodybalance.R
 import com.example.bodybalance.core.composable.BasicButton
+import com.example.bodybalance.core.composable.CustomTextField
 import com.example.bodybalance.player.composable.rememberExoPlayer
+import com.example.bodybalance.ui.theme.BodyBalanceTheme
 
 const val INTRODUCTION = "Introduction"
 
@@ -116,8 +124,75 @@ private fun IntroductionScreenLoading(modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun IntroductionPreview() {
-    Introduction(
-        modifier = Modifier.fillMaxSize(),
-        navToPlaylist = { Log.d("Preview", "Test button clicked") }
-    )
+    BodyBalanceTheme {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 16.dp)
+                .background(MaterialTheme.colorScheme.background)
+        ) {
+
+            Column(
+                modifier = Modifier.padding(horizontal = 16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                /*rememberExoPlayer(
+                context = LocalContext.current,
+                modifier = Modifier.fillMaxSize(),
+                videoUrl = "videoUrl",
+                listener = null
+            )*/
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .aspectRatio(16 / 9f)
+                        .background(Color.Gray),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text("ExoPlayer Placeholder", color = Color.White)
+                }
+                Text(
+                    modifier = Modifier
+                        .align(Alignment.Start)
+                        .padding(top = 16.dp),
+                    text = stringResource(R.string.introduction),
+                    textAlign = TextAlign.Start,
+                    fontSize = 22.sp,
+                    color = colorResource(R.color.white)
+                )
+                Text(
+                    modifier = Modifier
+                        .align(Alignment.Start)
+                        .padding(top = 16.dp),
+                    text = "Это видео поможет вам быстро разобраться, как всё работает\n" +
+                            "\n" +
+                            "После просмотра введите кодовое слово из видео, чтобы продолжить",
+                    textAlign = TextAlign.Start,
+                    fontSize = 14.sp,
+                    color = colorResource(R.color.white)
+                )
+                CustomTextField(
+                    modifier = Modifier
+                        .align(Alignment.Start)
+                        .padding(top = 24.dp),
+                    label = "Кодовое слово",
+                    onValueChange = {},
+                    clearAll = {}
+                )
+            }
+
+            BasicButton(
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .fillMaxWidth()
+                    .padding(bottom = 24.dp),
+                text = "Продолжить",
+                buttonColor = Color.Transparent,
+                textColor = MaterialTheme.colorScheme.primary,
+                onClick = { }
+            )
+
+
+        }
+    }
 }
