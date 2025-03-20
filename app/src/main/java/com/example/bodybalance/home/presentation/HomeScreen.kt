158692f5f1100a558df60ef0612fac085dedf2e6
Nikita <trenin.0235@gmail.com>
2025-03-20 21:13:56 +0700

    BottomSheet небольшие изменения

--- app/src/main/java/com/example/bodybalance/home/presentation/HomeScreen.kt
+++ app/src/main/java/com/example/bodybalance/home/presentation/HomeScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.BottomSheetDefaults
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
@@ -29,8 +28,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -126,16 +123,19 @@ fun HomeScreen(
                 ) {
                     Text(
                         text = "Получите логин после приёма врача",
-                        fontSize = 22.sp
+                        fontSize = 22.sp,
+                        letterSpacing = 0.sp,
                     )
                     Text(
-                        modifier = Modifier.padding(top = 12.dp),
+                        modifier = Modifier.padding(top = 12.dp, bottom = 14.dp),
                         text = "Body Balance— это программа спортивной реабилитации в формате ежедневных видео",
-                        fontSize = 14.sp
+                        fontSize = 14.sp,
+                        letterSpacing = 0.25.sp,
                     )
                     Text(
                         text = "Чтобы получить доступ к программе, запишитесь на приём к врачу",
-                        fontSize = 14.sp
+                        fontSize = 14.sp,
+                        letterSpacing = 0.25.sp,
                     )
                     BasicButton(
                         modifier = Modifier
