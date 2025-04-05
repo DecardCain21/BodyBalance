9f9141c7f70cca3598feeff1a704bdd655b13ac2
Nikita <trenin.0235@gmail.com>
2025-04-05 22:25:07 +0700

    Обработка TimeoutException

--- app/src/main/java/com/example/bodybalance/core/data/source/network/client/RetrofitNetworkClient.kt
+++ app/src/main/java/com/example/bodybalance/core/data/source/network/client/RetrofitNetworkClient.kt
@@ -1,22 +1,51 @@
 package com.example.bodybalance.core.data.source.network.client
 
+import android.net.http.HttpException
+import android.os.Build
+import android.util.Log
+import androidx.annotation.RequiresExtension
 import com.example.bodybalance.core.data.source.network.NetworkClient
 import com.example.bodybalance.core.data.source.network.NetworkError
 import com.example.bodybalance.core.util.getConnected
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.withContext
-import retrofit2.HttpException
+import java.net.SocketTimeoutException
 
+@RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
 abstract class RetrofitNetworkClient : NetworkClient {
 
+    private val maxRetries = 10
+    private val retryDelayMillis = 2000L
+
     override suspend fun <T> doRequest(request: suspend () -> T): Result<T> {
-        if (!getConnected()) { return Result.failure(NetworkError.NoInternet()) }
+
+        if (!getConnected()) {
+            return Result.failure(NetworkError.NoInternet())
+        }
+
+        var currentAttempt = 0
+
         return withContext(Dispatchers.IO) {
-            try {
-                Result.success(request())
-            } catch (e: HttpException) {
-                Result.failure(NetworkError.ServerError("", e.toString()))
+
+            while (currentAttempt < maxRetries) {
+                try {
+                    return@withContext Result.success(request())
+                } catch (e: HttpException) {
+                    return@withContext Result.failure(NetworkError.ServerError("", e.toString()))
+                } catch (e: SocketTimeoutException) {
+                    Log.e("SocketTimeoutException", "Попытка № $currentAttempt")
+                    currentAttempt++
+                    if (currentAttempt >= maxRetries) {
+                        return@withContext Result.failure(
+                            NetworkError.ServerError("", e.toString())
+                        )
+                    } else {
+                        delay(retryDelayMillis)
+                    }
+                }
             }
+            Result.failure(NetworkError.ServerError("", ""))
         }
     }
 }
