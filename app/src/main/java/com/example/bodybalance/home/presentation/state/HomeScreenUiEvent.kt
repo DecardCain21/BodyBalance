d1e367073a07fc8134e0dbf4bd1cb6cd87ca4738
Cain <decardcain21@gmail.com>
2025-06-07 22:28:20 +0300

    Upd links,add Dmitry link

--- app/src/main/java/com/example/bodybalance/home/presentation/state/HomeScreenUiEvent.kt
+++ app/src/main/java/com/example/bodybalance/home/presentation/state/HomeScreenUiEvent.kt
@@ -3,6 +3,6 @@ package com.example.bodybalance.home.presentation.state
 internal sealed interface HomeScreenUiEvent {
     data class InputLogin(val text: String) : HomeScreenUiEvent
     data object Enter : HomeScreenUiEvent
-    data object GetLogin : HomeScreenUiEvent
+    data class GetLogin(val link:String) : HomeScreenUiEvent
     data object ClearAll : HomeScreenUiEvent
 }
