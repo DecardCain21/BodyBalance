104951e8f81490f84281bfcfb25b169baa7b19a6
Cain <decardcain21@gmail.com>
2025-06-11 19:32:38 +0300

    minor changes this == 0L -> "0 MB"

--- app/src/main/java/com/example/bodybalance/core/util/LongExtension.kt
+++ app/src/main/java/com/example/bodybalance/core/util/LongExtension.kt
@@ -7,7 +7,7 @@ public fun Long.convertToFileSize(): String {
     require(this >= 0) { "File size cannot be negative" }
 
     return when {
-        this == 0L -> "0 B"
+        this == 0L -> "0 MB"
         this < 1024 -> "$this B"
         this < 1024 * 1024 -> "%.1f KB".format(this / 1024.0)
         this < 1024 * 1024 * 1024 -> "%.1f MB".format(this / (1024.0 * 1024.0))
